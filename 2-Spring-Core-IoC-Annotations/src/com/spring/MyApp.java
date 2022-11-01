67c90c7af1e8182d3fc610ee838a162ee87e49e7
AbdullahAlhussein <Abdullah30Alhussein@gmail.com>
2022-11-01 07:45:37 +0300

    delete out-ioc files

--- 2-Spring-Core-IoC-Annotations/src/com/spring/MyApp.java
+++ 2-Spring-Core-IoC-Annotations/src/com/spring/MyApp.java
@@ -6,19 +6,17 @@ public class MyApp {
 
     public static void main(String [] arges){
 
-
         // read the spring configuration file , create a Spring container
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         // get bean from spring container
-        Coach thecoach = context.getBean("theBaseballCoach", Coach.class);
+        Coach theCoach = context.getBean("theBaseballCoach", Coach.class);
 
         // call methods on the bean
-        System.out.println(thecoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyWorkout());
 
         // close the context
         context.close();
-
 
     }
 }
