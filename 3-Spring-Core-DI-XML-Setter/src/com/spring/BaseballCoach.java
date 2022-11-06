66d9ce15f49388bdf75adab91d9625fabef89d61
AbdullahAlhussein <Abdullah30Alhussein@gmail.com>
2022-11-06 15:07:01 +0300

    DI-XML-Setter-tutorial

---
+++ 3-Spring-Core-DI-XML-Setter/src/com/spring/BaseballCoach.java
@@ -0,0 +1,30 @@
+package com.spring;
+
+public class BaseballCoach implements Coach{
+
+    // define a private field for the dependency
+    public FortuneService fortuneService;
+
+    //create a no-argument constructor
+    public BaseballCoach(){
+        System.out.println("BaseballCoach inside no-argument constructor ");
+    }
+
+    // our setter for dependency injection
+    public void setFortuneService(FortuneService fortuneService){
+        System.out.println("BaseballCoach inside method - setFortuneService ");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Spend 30 minutes on batting practice";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        // use my fortuneService to get a fortune
+        return fortuneService.getFortune();
+    }
+
+}
