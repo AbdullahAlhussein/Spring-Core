351700089e66edcc1c43db416ac1450ec6b5f24b
AbdullahAlhussein <Abdullah30Alhussein@gmail.com>
2022-11-07 14:45:05 +0300

    DI-Annotations-Setter

---
+++ 8-Spring-Core-DI-Annotations-Setter/src/com/spring/MyApp.java
@@ -0,0 +1,26 @@
+package com.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApp {
+
+    public static void main(String [] arges){
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("baseballCoach", Coach.class);
+
+        // call method on the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        // let's call our new method for fortunes
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+
+
+    }
+}
