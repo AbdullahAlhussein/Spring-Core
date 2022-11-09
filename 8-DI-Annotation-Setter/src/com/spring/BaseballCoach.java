374083604ec7746d69840ff53c10d6c22b3f66cf
AbdullahAlhussein <Abdullah30Alhussein@gmail.com>
2022-11-09 12:03:09 +0300

    add DI-Annotation-Setter tutorial

---
+++ 8-DI-Annotation-Setter/src/com/spring/BaseballCoach.java
@@ -0,0 +1,35 @@
+package com.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach{
+
+    // define a private field for the dependency
+    private FortuneService fortuneService;
+
+    //create a no-argument constructor
+    public BaseballCoach(){
+        System.out.println(">> BaseballCoach inside no-argument constructor");
+    }
+
+    // our setter for dependency injection
+    @Autowired
+    public void setFortuneService(FortuneService fortuneService){
+        System.out.println(">> BaseballCoach inside method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return ">> Spend 30 minutes on batting practice";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        // use my fortuneService to get a fortune
+        return fortuneService.getFortune();
+    }
+
+}
---
+++ 8-DI-Annotation-Setter/src/com/spring/HappyFortunneService.java
@@ -0,0 +1,17 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortunneService implements FortuneService{
+
+    //create a no-argument constructor
+    public HappyFortunneService(){
+        System.out.println(">> HappyFortunneService inside no-argument constructor");
+    }
+
+    @Override
+    public String getFortune() {
+        return ">> Today is your lucky day!";
+    }
+}
