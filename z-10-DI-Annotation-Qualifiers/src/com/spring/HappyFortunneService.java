e22a829880fa3eaccc943710997387800d4bd2a1
AbdullahAlhussein <Abdullah30Alhussein@gmail.com>
2022-11-09 17:42:52 +0300

    add DI-Annotation-Qualifiers tutorial

---
+++ z-10-DI-Annotation-Qualifiers/src/com/spring/HappyFortunneService.java
@@ -0,0 +1,16 @@
+package com.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HappyFortunneService implements FortuneService{
+
+    public HappyFortunneService(){
+        System.out.println(">> HappyFortunneService inside no-argument constructor");
+    }
+
+    @Override
+    public String getFortune() {
+        return ">> Today is your lucky day! + HappyFortunneService";
+    }
+}
